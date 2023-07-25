26e377473ece10322d21abc54c0202ac6ed2dc99
hakan pesener <pesener@windowslive.com>
2023-07-25 18:36:27 +0300

    set up SavedShows add it to  account and set up account page add delete button to saved shows

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <>
+      <div className="w-full text-white">
+        <img
+          className="  w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d7af077c-af5a-4055-8f9a-740a43588583/d08d5065-cd5d-4962-b1d2-cd72e271efb9/TR-en-20230717-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        ></img>
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-xl md:text-5xl font-bold">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
 };
 
 export default Account;
